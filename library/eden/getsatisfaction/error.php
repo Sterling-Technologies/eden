8f6426ed8f6e0e20bb0031359cca7456fb87fc05
cblanquera <cblanquera@38ede93f-50f4-44af-b0b3-68ccca78c181>
2011-12-13 06:31:45 +0000

    gs dev
    git-svn-id: http://svn.openovate.com/edenv2/trunk@16 38ede93f-50f4-44af-b0b3-68ccca78c181

---
+++ library/eden/getsatisfaction/error.php
@@ -0,0 +1,42 @@
+<?php //-->
+/*
+ * This file is part of the Eden package.
+ * (c) 2009-2011 Christian Blanquera <cblanquera@gmail.com>
+ *
+ * Copyright and license information can be found at LICENSE.txt
+ * distributed with this package.
+ */
+
+/**
+ * Get Satisfaction Errors
+ *
+ * @package    Eden
+ * @category   google
+ * @author     Christian Blanquera <cblanquera@gmail.com>
+ * @version    $Id: exception.php 1 2010-01-02 23:06:36Z blanquera $
+ */
+class Eden_Getsatisfaction_Error extends Eden_Error {
+	/* Constants
+	-------------------------------*/
+	/* Public Properties
+	-------------------------------*/
+	/* Protected Properties
+	-------------------------------*/
+	/* Private Properties
+	-------------------------------*/
+	/* Get
+	-------------------------------*/
+	public static function get($message = NULL, $code = 0) {
+		$class = __CLASS__;
+		return new $class($message, $code);
+	}
+	
+	/* Magic
+	-------------------------------*/
+	/* Public Methods
+	-------------------------------*/
+	/* Protected Methods
+	-------------------------------*/
+	/* Private Methods
+	-------------------------------*/
+}
