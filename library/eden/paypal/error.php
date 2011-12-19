42281f8727d7d1e50c2a8d559204e66f1afba3ec
sy <sy@38ede93f-50f4-44af-b0b3-68ccca78c181>
2011-12-19 08:29:38 +0000

    git-svn-id: http://svn.openovate.com/edenv2/trunk@19 38ede93f-50f4-44af-b0b3-68ccca78c181

--- library/eden/paypal/error.php
+++ library/eden/paypal/error.php
@@ -1,10 +1,42 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<title>Untitled Document</title>
-</head>
+<?php //-->
+/*
+ * This file is part of the Eden package.
+ * (c) 2009-2011 Christian Blanquera <cblanquera@gmail.com>
+ *
+ * Copyright and license information can be found at LICENSE.txt
+ * distributed with this package.
+ */
 
-<body>
-</body>
-</html>
+/**
+ * Eventbrite Errors
+ *
+ * @package    Eden
+ * @category   Paypal
+ * @author     Christian Blanquera <cblanquera@gmail.com>
+ * @version    $Id: exception.php 1 2010-01-02 23:06:36Z blanquera $
+ */
+class Eden_Paypal_Error extends Eden_Error {
+	/* Constants
+	-------------------------------*/
+	/* Public Properties
+	-------------------------------*/
+	/* Protected Properties
+	-------------------------------*/
+	/* Private Properties
+	-------------------------------*/
+	/* Get
+	-------------------------------*/
+	public static function get($message = NULL, $code = 0) {
+		$class = __CLASS__;
+		return new $class($message, $code);
+	}
+	
+	/* Magic
+	-------------------------------*/
+	/* Public Methods
+	-------------------------------*/
+	/* Protected Methods
+	-------------------------------*/
+	/* Private Methods
+	-------------------------------*/
+}
