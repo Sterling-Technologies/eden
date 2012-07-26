2d4e11449677da1bb0b7dc53478c32390f4449ee
sy <sy@38ede93f-50f4-44af-b0b3-68ccca78c181>
2012-07-26 07:33:53 +0000

    added google factory class
    git-svn-id: http://svn.openovate.com/edenv2/trunk@195 38ede93f-50f4-44af-b0b3-68ccca78c181

--- library/eden/google.php
+++ library/eden/google.php
@@ -6,110 +6,79 @@
  * Copyright and license information can be found at LICENSE.txt
  * distributed with this package.
  */
-
 require_once dirname(__FILE__).'/curl.php';
 require_once dirname(__FILE__).'/google/error.php';
-require_once dirname(__FILE__).'/google/oauth.php';
 require_once dirname(__FILE__).'/google/base.php';
-require_once dirname(__FILE__).'/google/calendar.php';
+require_once dirname(__FILE__).'/google/calendar/acl.php';
+require_once dirname(__FILE__).'/google/calendar/calendars.php';
+require_once dirname(__FILE__).'/google/calendar/color.php';
+require_once dirname(__FILE__).'/google/calendar/event.php';
+require_once dirname(__FILE__).'/google/calendar/freebusy.php';
+require_once dirname(__FILE__).'/google/calendar/list.php';
+require_once dirname(__FILE__).'/google/calendar/settings.php';
+require_once dirname(__FILE__).'/google/drive/about.php';
+require_once dirname(__FILE__).'/google/drive/apps.php';
+require_once dirname(__FILE__).'/google/drive/changes.php';
+require_once dirname(__FILE__).'/google/drive/children.php';
+require_once dirname(__FILE__).'/google/drive/files.php';
+require_once dirname(__FILE__).'/google/drive/parent.php';
+require_once dirname(__FILE__).'/google/drive/permissions.php';
+require_once dirname(__FILE__).'/google/drive/revisions.php';
 
 /**
  * Google API factory. This is a factory class with 
- * methods that will load up different Google classes.
+ * methods that will load up different google classes.
  * Google classes are organized as described on their 
- * developer site: calendar, docs, youtube, shortener
- * gmail, contacts, maps, google+. We also added several 
- * classes for more advanced options when posting to Google.
+ * developer site: calendar and drive.
  *
  * @package    Eden
  * @category   google
- * @author     Christian Blanquera cblanquera@openovate.com
+ * @author     Christian Symon M. Buenavista sbuenavista@openovate.com
  */
 class Eden_Google extends Eden_Class {
-	/* Constants
+	/* Constants 
 	-------------------------------*/
-	const CALENDAR_SCOPE	= 'https://www.googleapis.com/auth/calendar';
-	const PLUS_SCOPE		= 'https://www.googleapis.com/auth/plus.me';
-	
 	/* Public Properties
 	-------------------------------*/
 	/* Protected Properties
 	-------------------------------*/
 	/* Private Properties
 	-------------------------------*/
-	/* Magic
+	/* Get
 	-------------------------------*/
 	public static function i() {
 		return self::_getSingleton(__CLASS__);
 	}
-	
+	/* Magic
+	-------------------------------*/
 	/* Public Methods
 	-------------------------------*/
 	/**
-	 * Returns Google OAuth
+	 * Returns google drive methods
 	 *
-	 * @param string
-	 * @param string
-	 * @param string
-	 * @return Eden_Google_Oauth
+	 * @param *string 
+	 * @return Eden_Google_Drive
 	 */
-	public function auth($key, $secret, $redirect, $apiKey) {
-		Eden_Google_Error::i()
-			->argument(1, 'string')
-			->argument(2, 'string')
-			->argument(3, 'string')
-			->argument(4, 'string');
-			
-		return Eden_Google_Oauth::i($key, $secret, $redirect, $apiKey);
+	public function drive($token) {
+		//Argument 1 must be a string
+		Eden_Google_Error::i()->argument(1, 'string');
+		
+		return Eden_Google_Drive::i($token);
 	}
 	
 	/**
-	 * Returns Google Calendar
+	 * Returns google calendar methods
 	 *
-	 * @param string
-	 * @return Eden_Google_Calendar
+	 * @param *string 
+	 * @return Eden_Google_Drive
 	 */
 	public function calendar($token) {
+		//Argument 1 must be a string
 		Eden_Google_Error::i()->argument(1, 'string');
-			
+		
 		return Eden_Google_Calendar::i($token);
 	}
 	
-	/**
-	 * Returns Google Docs
-	 *
-	 * @param string
-	 * @return Eden_Google_Docs
-	 */
-	public function docs($token) {
-		Eden_Google_Error::i()->argument(1, 'string');
-			
-		return Eden_Google_Docs::i($token);
-	}
-	
-	/**
-	 * Returns Google Plus
-	 *
-	 * @param string
-	 * @return Eden_Google_Docs
-	 */
-	public function plus($token) {
-		Eden_Google_Error::i()->argument(1, 'string');
-			
-		return Eden_Google_Plus::i($token);
-	}
-	
-	/**
-	 * Returns Google Shopping
-	 *
-	 * @param string
-	 * @return Eden_Google_Shopping
-	 */
-	public function shopping($key) {
-		Eden_Google_Error::i()->argument(1, 'string');
-		
-		return Eden_Google_Shopping::i($key);
-	}
 	/* Protected Methods
 	-------------------------------*/
 	/* Private Methods
