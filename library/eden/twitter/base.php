d953633f0e73ef3c79f794a5847912d1f7e828eb
sbuenavista <sbuenavista@openovate.com>
2012-08-24 17:46:13 +0800

    Update library/eden/twitter/base.php
    fixed update media

--- library/eden/twitter/base.php
+++ library/eden/twitter/base.php
@@ -25,6 +25,13 @@ class Eden_Twitter_Base extends Eden_Oauth_Base {
 	protected $_consumerSecret 	= NULL;
 	protected $_accessToken		= NULL;
 	protected $_accessSecret	= NULL;
+	protected $_signingKey		= NULL;
+	protected $_baseString		= NULL;
+	protected $_signingParams	= NULL;
+	protected $_url				= NULL;
+	protected $_authParams 		= NULL;
+	protected $_authHeader 		= NULL;
+	protected $_headers	 		= NULL;
 	
 	/* Private Properties
 	-------------------------------*/
@@ -90,6 +97,7 @@ class Eden_Twitter_Base extends Eden_Oauth_Base {
 		
 		$rest = Eden_Oauth::i()
 			->getConsumer($url, $this->_consumerKey, $this->_consumerSecret)
+			->setMethodToPost()
 			->setToken($this->_accessToken, $this->_accessSecret)
 			->setSignatureToHmacSha1();
 		
@@ -104,9 +112,6 @@ class Eden_Twitter_Base extends Eden_Oauth_Base {
 		//prevent sending fields with no value
 		$query = $this->_accessKey($query);
 		
-		$headers = array();
-		$headers[] = Eden_Oauth_Consumer::POST_HEADER;
-		
 		$rest = Eden_Oauth::i()
 			->getConsumer($url, $this->_consumerKey, $this->_consumerSecret)
 			->setMethodToPost()
@@ -117,10 +122,13 @@ class Eden_Twitter_Base extends Eden_Oauth_Base {
 		$signature 		= $rest->getSignature($query);
 		$authorization 	= $rest->getAuthorization($signature, false);
 		$authorization 	= $this->_buildQuery($authorization);
-		
+	
 		if(is_array($query)) {
 			$query 	= $this->_buildQuery($query);
 		}
+
+		$headers = array();
+		$headers[] = Eden_Oauth_Consumer::POST_HEADER;
 		
 		//determine the conector
 		$connector = NULL;
@@ -135,7 +143,6 @@ class Eden_Twitter_Base extends Eden_Oauth_Base {
 		
 		//now add the authorization to the url
 		$url .= $connector.$authorization;
-		
 		//set curl
 		$curl = Eden_Curl::i()
 			->verifyHost(false)
@@ -146,17 +153,113 @@ class Eden_Twitter_Base extends Eden_Oauth_Base {
 			->setHeaders($headers);
 		
 		//get the response
-		$response = $curl->getResponse();
+		$response = $curl->getJsonResponse();
 		
 		$this->_meta 					= $curl->getMeta();
 		$this->_meta['url'] 			= $url;
 		$this->_meta['authorization'] 	= $authorization;
 		$this->_meta['headers'] 		= $headers;
 		$this->_meta['query'] 			= $query;
-
+		
 		return $response;
 	}
 	
+	protected function _upload($url, array $query = array()) { exit;
+		//prevent sending fields with no value
+		$query = $this->_accessKey($query);
+		//set url
+		$this->_url = $url;
+		//make authorization for twitter
+		$this->_getAuthentication();
+		//set headers
+		$this->_headers['Expect'] = '';
+			//at this point, the authentication header si already set
+			foreach($this->_headers as $k => $v) {
+				//trim header
+				$headers[] = trim($k . ': ' . $v);
+			}
+		 //set curl
+		$curl = Eden_Curl::i()
+			->verifyHost(false)
+			->verifyPeer(false)
+			->setUrl($url)
+			->setPost(true)
+			->setPostFields($query)	
+			->setHeaders($headers);
+		//json decode the response	
+		$response = $curl->getJsonResponse();
+		
+		$this->_meta 					= $curl->getMeta();
+		$this->_meta['url'] 			= $url;
+		$this->_meta['headers'] 		= $headers;
+		$this->_meta['query'] 			= $query;
+		
+		return $response;
+	}
+	
+	protected function _getAuthentication() { 
+		//populate fields
+		$defaults = array(
+		  'oauth_version'          	=> '1.0',
+		  'oauth_nonce'            	=> md5(uniqid(rand(), true)),
+		  'oauth_timestamp'        	=> time(),
+		  'oauth_consumer_key'     	=> $this->_consumerKey,
+		  'oauth_signature_method' 	=> 'HMAC-SHA1',
+		  'oauth_token'				=> $this->_accessToken);
+		
+		//encode the parameters
+		foreach ($defaults as $k => $v) {
+		  $this->_signingParams[$this->safeEncode($k)] = $this->safeEncode($v);
+		}
+		//sort an array by keys using a user-defined comparison function
+		uksort($this->_signingParams, 'strcmp'); 
+		
+		foreach ($this->_signingParams as $k => $v) {
+		  //encode key 
+		  $k = $this->safeEncode($k);
+		  //encode value
+		  $v = $this->safeEncode($v);
+		  $_signing_params[$k] = $v;
+		  $kv[] = "{$k}={$v}";
+		}
+		//implode signingParams to make it a string
+		$this->_signingParams = implode('&', $kv);
+   		//check if they have the same value
+    	$this->_authParams = array_intersect_key($defaults, $_signing_params);
+		//make a base string
+		$base = array('POST', $this->_url, $this->_signingParams);
+		//convert array to string and safely encode it 
+		$this->_baseString = implode('&', $this->safeEncode($base));
+		//make a signing key by combining consumer secret and access secret
+		$this->_signingKey = $this->safeEncode($this->_consumerSecret).'&'.$this->safeEncode($this->_accessSecret);
+		//generate signature
+		$this->_authParams['oauth_signature'] = $this->safeEncode(
+       	 	base64_encode(hash_hmac('sha1', $this->_baseString, $this->_signingKey, true)));
+		
+		foreach ($this->_authParams as $k => $v) {
+		  $kv[] = "{$k}=\"{$v}\"";
+		}
+		//implode authHeader to make it ia string
+		$this->_authHeader = 'OAuth ' . implode(', ', $kv);
+		//put it in the authorization headera
+		$this->_headers['Authorization'] = $this->_authHeader;
+  	} 
+ 	
+	protected function safeEncode($data) {
+		//if data is in array
+		if (is_array($data)) {
+			//array map the data
+			return array_map(array($this, 'safeEncode'), $data);
+		//else it is not array
+		} else if (is_scalar($data)) {
+		  //str ireplace data, it is case-insensitive version of str_replace()
+		  return str_ireplace(array('+', '%7E'), array(' ', '~'), rawurlencode($data));
+		//else it is null or uempty
+		} else {
+		  return '';
+		}
+	
+	}
 	/* Private Methods
 	-------------------------------*/
 }
